08e4e75633e82ab1448911a736bf7268398e17a6
George <gosha.aladin@gmail.com>
2023-06-12 02:59:19 +0300

    gh-pages

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import Vue from "vue";
-import App from './App.vue'
-import './js/functions'
-import './assets/scss/main.scss'
-
-const MyApp = createApp(App);
-MyApp.mount('#app');
